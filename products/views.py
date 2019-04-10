2e7dd0afcd01e2de3074f8466f751d5bd1c0e350
MadiyorAbdukhashimov <tashkent.1997.1.7@gmail.com>
2019-04-10 22:00:25 +0500

    before adding slug field

--- products/views.py
+++ products/views.py
@@ -11,7 +11,22 @@ class ProductList(ListView):
     queryset = Product.objects.all()
     template_name = 'products/list.html'
 
+
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
-    template_name = 'products/detail.html'
+    template_name = "products/detail.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(
+            *args, **kwargs)
+        print(context)
+        # context['abc'] = 123
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
